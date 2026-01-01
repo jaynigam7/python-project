ade4c0832f762957c2688967ad44d6d9586d9900
jay_nigam <Nigamjay57@gmail.com>
2026-01-01 10:53:11 +0530

    Day-1 Pandas

---
+++ practice/assignment-1.py
@@ -0,0 +1,15 @@
+# q1
+# print("Hello")
+# q2
+# print("HELLO")
+# q3
+# print("Hello world")
+# q4
+# a=input("Enter the name: ")
+# print(a)
+# q5
+# a=input("Enter the middle name: ")
+# print(a)
+# q6
+# a=input("Enter the last name: ")
+# print(a)
